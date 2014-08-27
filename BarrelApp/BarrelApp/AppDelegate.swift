82e553f7e432c40d62d1c33f44133c495d71e428
Thanos Siopoudis <siopoudis@me.com>
2014-08-27 09:31:55 +0100

    Setup the game folder in AppDelegate

--- BarrelApp/BarrelApp/AppDelegate.swift
+++ BarrelApp/BarrelApp/AppDelegate.swift
@@ -9,19 +9,300 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
+    enum BLErrorCodes:Int {
+        case BLGamesFolderURLInvalid
+    };
+    
     @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+    var _gamesFolder:NSURL?
+    var gamesFolderURL:NSURL? {
+        get {
+            if (self._gamesFolder == nil) {
+                var returnVal:NSURL?
+                var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults();
+                var bookmarkData:NSData? = userDefaults.dataForKey("gamesFolderURLBookmark");
+                if (bookmarkData != nil) {
+                    var dataIsStale:ObjCBool = false;
+                    var resolutionError:NSError? = nil;
+                    var folderURL:NSURL? = NSURL.URLByResolvingBookmarkData(bookmarkData,
+                        options: NSURLBookmarkResolutionOptions.WithoutUI,
+                        relativeToURL: nil,
+                        bookmarkDataIsStale: &dataIsStale,
+                        error: &resolutionError);
+                    
+                    if (folderURL != nil) {
+                        returnVal = folderURL?.fileReferenceURL()?.copy() as? NSURL;
+                        
+                        if (dataIsStale) {
+                            var updatedBookmarkData:NSData? = folderURL?.bookmarkDataWithOptions(NSURLBookmarkCreationOptions.allZeros,
+                                includingResourceValuesForKeys: nil,
+                                relativeToURL: nil,
+                                error: nil
+                            );
+                            if (updatedBookmarkData != nil) {
+                                userDefaults.setObject(updatedBookmarkData!, forKey: "gamesFolderURLBookmark");
+                            }
+                        }
+                    }
+                }
+                self._gamesFolder = returnVal;
+            }
+            
+            return self._gamesFolder;
+        }
+        set {
+            var newURL:NSURL? = newValue?.fileReferenceURL();
+            if (newURL != self._gamesFolder) {
+                self.willChangeValueForKey("gamesFolderURL");
+                self._gamesFolder = newURL?.fileReferenceURL();
+                self.didChangeValueForKey("gamesFolderURL");
+                
+                // Store the value in NSUserDefaults
+                if (newURL != nil) {
+                    var bookmarkData:NSData? = newURL?.bookmarkDataWithOptions(NSURLBookmarkCreationOptions.allZeros,
+                        includingResourceValuesForKeys: nil,
+                        relativeToURL: nil,
+                        error: nil
+                    );
+                    
+                    if (bookmarkData != nil) {
+                        NSUserDefaults.standardUserDefaults().setObject(bookmarkData!, forKey: "gamesFolderURLBookmark");
+                    }
+                }
+            }
+        }
+    }
+    
+    override class func initialize() {
+        AppDelegate.prepareValueTransformers();
+    }
+    
+    override init() {
+        super.init();
+    }
+    
+    func applicationWillFinishLaunching(notification: NSNotification!) {
+        if (self.gamesFolderURL == nil && self.gamesFolderChosen() == false) {
+            var defaultURL:NSURL = AppDelegate.preferredGamesFolderURL();
+            var nilError:NSError? = nil;
+            self.assignGamesFolderURL(defaultURL, addSampleGames: true, createIfMissing: true, outError: &nilError);
+        }
     }
 
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
+    class func prepareValueTransformers() {
+        let pathTransformer:BLIconifiedDisplayPathTransformer = BLIconifiedDisplayPathTransformer(joiner: " ▸ ", ellipsis: "",  maxComponents: 0);
+        pathTransformer.missingFileIcon = NSImage(named: "gamefolder");
+        pathTransformer.hidesSystemRoots = true;
         
+        var pathStyle:NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as NSMutableParagraphStyle;
+        pathStyle.lineBreakMode = NSLineBreakMode.ByTruncatingMiddle;
+        pathTransformer.textAttributes?.setObject(pathStyle, forKey: NSParagraphStyleAttributeName);
+        NSValueTransformer.setValueTransformer(pathTransformer, forName: "BLIconifiedGamesFolderPath");
+    }
+    
+    // MARK: - Type Methods
+    class func preferredGamesFolderURL() -> NSURL {
+        return self.commonGamesFolderURLs()[0] as NSURL;
+    }
+    
+    class func commonGamesFolderURLs() -> NSArray {
+        var URLs:NSArray? = nil;
+        var onceToken:dispatch_once_t = 0
+        dispatch_once(&onceToken, {
+            var defaultName:NSString = "Windows Games";
+            var manager:NSFileManager = NSFileManager.defaultManager();
+            
+            var homeURL:NSURL = NSURL(fileURLWithPath: NSHomeDirectory());
+            var docsURL:NSURL = manager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask)[0] as NSURL;
+            var userAppURL:NSURL = manager.URLsForDirectory(NSSearchPathDirectory.ApplicationDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask)[0] as NSURL;
+            var appURL:NSURL = manager.URLsForDirectory(NSSearchPathDirectory.ApplicationDirectory, inDomains: NSSearchPathDomainMask.LocalDomainMask)[0] as NSURL;
+            
+            URLs = [
+                homeURL.URLByAppendingPathComponent(defaultName),
+                docsURL.URLByAppendingPathComponent(defaultName),
+                userAppURL.URLByAppendingPathComponent(defaultName),
+                appURL.URLByAppendingPathComponent(defaultName)
+            ];
+        });
+        
+        return URLs!;
+    }
+    
+    class func reservedURLs() -> NSSet {
+        var reservedURLs:NSMutableSet? = nil;
+        var onceToken:dispatch_once_t = 0;
+        dispatch_once(&onceToken, {
+            var manager:NSFileManager = NSFileManager.defaultManager();
+            if let homeURLs = NSURL.fileURLWithPath(NSHomeDirectory()) {
+                reservedURLs = NSMutableSet(objects: homeURLs);
+            }
+            
+            let NUM_DIRS = 7;
+            let reservedDirs:[NSSearchPathDirectory] = [
+                NSSearchPathDirectory.DocumentDirectory,
+                NSSearchPathDirectory.AllApplicationsDirectory,
+                NSSearchPathDirectory.AllLibrariesDirectory,
+                NSSearchPathDirectory.DesktopDirectory,
+                NSSearchPathDirectory.DownloadsDirectory,
+                NSSearchPathDirectory.UserDirectory,
+                NSSearchPathDirectory.SharedPublicDirectory
+            ];
+            
+            for var i = 0; i < NUM_DIRS; i++ {
+                var searchURLs:NSArray = manager.URLsForDirectory(reservedDirs[i], inDomains: NSSearchPathDomainMask.AllDomainsMask);
+                reservedURLs?.addObjectsFromArray(searchURLs);
+            }
+        });
+        
+        return reservedURLs!;
+    }
+    
+    private class func _isReservedURL(URL:NSURL) -> Bool {
+        // Reject reserved paths
+        if (AppDelegate.reservedURLs().containsObject(URL)) {
+            return true;
+        }
+        
+        var manager:NSFileManager = NSFileManager.defaultManager();
+        
+        // Reject paths located inside system library folders (though we allow them within the user's own Library folder)
+        var libraryURLs:NSArray = manager.URLsForDirectory(NSSearchPathDirectory.AllLibrariesDirectory,
+                                                            inDomains: NSSearchPathDomainMask.LocalDomainMask | NSSearchPathDomainMask.SystemDomainMask);
+        for libraryURL in libraryURLs {
+            if (URL.isBasedInURL(libraryURL as? NSURL)) {
+                return true;
+            }
+        }
+        
+        // Reject base home folder paths (though accept any folder withing them, of course)
+        var userDirectoryURLs = manager.URLsForDirectory(NSSearchPathDirectory.UserDirectory, inDomains: NSSearchPathDomainMask.AllDomainsMask);
+        var parentURL = URL.URLByDeletingLastPathComponent!;
+        for userDirectoryURL in userDirectoryURLs {
+            if (parentURL.isEqual(userDirectoryURL)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    class func keyPathsForValuesAffectingGamesFolderIcon() -> NSSet! {
+        return NSSet(objects: "gamesFolderURL");
+    }
+    
+    override class func automaticallyNotifiesObserversForKey(key: String!) -> Bool {
+        return true;
+    }
+    
+    func gamesFolderChosen() -> Bool {
+        var defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults();
+        if (defaults.dataForKey("gamesFolderURLBookmark") != nil) {
+            return true;
+        }
+        
+        if (defaults.dataForKey("gamesFolder") != nil) {
+            return true;
+        }
+        
+        return false;
     }
 
-
+    func assignGamesFolderURL(URL:NSURL?, addSampleGames:Bool, createIfMissing:Bool, inout outError:NSError?) -> Bool {
+        assert(URL != nil, "nil URL provided to assignGamesFolderURL:addSampleGames:createIfMissing:error:");
+        
+        var theURL = URL!;
+        
+        var reachabilityError:NSError? = nil;
+        if (!theURL.checkResourceIsReachableAndReturnError(&reachabilityError)) {
+            if (createIfMissing) {
+                var created:Bool = NSFileManager.defaultManager().createDirectoryAtURL(theURL, withIntermediateDirectories: true, attributes: nil, error: &outError);
+                if (!created) {
+                    return false;
+                }
+            }
+            else {
+                if (outError != nil) {
+                    outError = reachabilityError;
+                }
+                return false;
+            }
+        }
+        
+        var isValid:Bool = self.validateGamesFolderURL(&theURL, outError: &outError);
+        if (!isValid) {
+            return false;
+        }
+        
+        // If we got this far, we can go ahead and assign this as our games folder path.
+        // Apply the requested options now
+        if (addSampleGames) {
+            // Add the sample games here
+        }
+        
+        self.gamesFolderURL = URL;
+        return true;
+    }
+    
+    func validateGamesFolderURL(inout ioValue:NSURL, inout outError:NSError?) -> Bool {
+        var URL:NSURL? = ioValue;
+        
+        // Accept nil paths, since these will clear the preference
+        if (URL == nil) {
+            return true;
+        }
+        
+        URL = URL?.URLByStandardizingPath;
+        
+        if (AppDelegate._isReservedURL(URL!)) {
+            if (outError != nil) {
+                var displayName:AnyObject?
+                
+                URL?.getResourceValue(&displayName, forKey: NSURLLocalizedNameKey, error: nil);
+                if (displayName == nil) {
+                    displayName = URL?.lastPathComponent;
+                }
+                
+                var description:String = "\(displayName) is a special OS X folder and not suitable for storing your Windows games.";
+                var userInfo:NSDictionary = [
+                    NSLocalizedDescriptionKey: description,
+                    NSLocalizedRecoverySuggestionErrorKey: "Please create a subfolder, or choose a different folder your have created yourself",
+                    NSURLErrorKey: URL!
+                ];
+                
+                outError = NSError.errorWithDomain("BLGamesFolderErrorDomain", code: BLErrorCodes.BLGamesFolderURLInvalid.toRaw(), userInfo: userInfo);
+            }
+            
+            return false;
+        }
+        
+        // Warn if we do not currently have write permission to access that URL
+        var writeableFlag:AnyObject?
+        URL?.getResourceValue(&writeableFlag, forKey: NSURLIsWritableKey, error: nil);
+        
+        if (!writeableFlag!.boolValue) {
+            if (outError != nil) {
+                var displayName:AnyObject?
+                
+                URL?.getResourceValue(&displayName, forKey: NSURLLocalizedNameKey, error: nil);
+                if (displayName == nil) {
+                    displayName = URL?.lastPathComponent;
+                }
+                
+                var description:String = "Barrel cannot write to the \(displayName)folder.";
+                var userInfo:NSDictionary = [
+                    NSLocalizedDescriptionKey: description,
+                    NSLocalizedRecoverySuggestionErrorKey: "Please check the file permissions, or choose a different folder.",
+                    NSURLErrorKey: URL!
+                ];
+                
+                outError = NSError.errorWithDomain(NSCocoaErrorDomain, code: NSFileWriteNoPermissionError, userInfo: userInfo);
+            }
+            
+            return false;
+        }
+        
+        // If we got this far, the URL is OK
+        return true;
+    }
 }
 
