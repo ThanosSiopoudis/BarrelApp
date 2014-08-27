95878f13c7195bc97ecf687821e996b1736ff179
Thanos Siopoudis <siopoudis@me.com>
2014-08-27 09:30:18 +0100

    Add a controller for the games folder NSOpenPanel

---
+++ BarrelApp/BarrelApp/Classes/SubClasses/BLPreferences/BLGamesFolderPanelController.swift
@@ -0,0 +1,76 @@
+//
+//  BLGamesFolderPanelController.swift
+//  BarrelApp
+//
+//  Created by Thanos Siopoudis on 26/08/2014.
+//  Copyright (c) 2014 AppCake Limited. All rights reserved.
+//
+
+import Cocoa
+
+class BLGamesFolderPanelController : NSViewController, NSOpenSavePanelDelegate {
+    @IBOutlet
+    var sampleGamesToggle:NSButton!
+    
+    override convenience init() {
+        self.init(nibName: "GamesFolderPanelOptions", bundle: nil);
+    }
+    
+    func showGamesFolderPanelWindow(window:NSWindow!) {
+        var openPanel:NSOpenPanel = NSOpenPanel();
+        var dlg:AppDelegate = NSApplication.sharedApplication().delegate as AppDelegate;
+        
+        var currentFolderURL:NSURL?
+        if let gamesURL = dlg.gamesFolderURL {
+            currentFolderURL = gamesURL;
+        }
+        var initialURL:NSURL?
+        if let cfURL = currentFolderURL {
+            initialURL = cfURL;
+        }
+        else {
+            initialURL = NSURL(fileURLWithPath: NSHomeDirectory());
+        }
+        
+        openPanel.delegate = self;
+        openPanel.canCreateDirectories = true;
+        openPanel.canChooseDirectories = true;
+        openPanel.canChooseFiles = false;
+        openPanel.treatsFilePackagesAsDirectories = false;
+        openPanel.allowsMultipleSelection = false;
+        
+        openPanel.accessoryView = self.view;
+        openPanel.directoryURL = initialURL;
+        
+        openPanel.prompt = "Select";
+        openPanel.message = "Select a folder in which to keep your Windows games:";
+        
+        self.sampleGamesToggle.state = true;
+        
+        if (window != nil) {
+            openPanel.beginSheetModalForWindow(window, completionHandler: {(result) -> Void in
+                if (result == NSFileHandlingPanelOKButton) {
+                    var folderError:NSError?
+                    var assigned:Bool = self.chooseGamesFolderURL(openPanel.URL, outError: &folderError);
+                    
+                    if (!assigned && folderError != nil) {
+                        if (window != nil) {
+                            openPanel.orderOut(self);
+                            self.presentError(folderError, modalForWindow: window, delegate: nil, didPresentSelector: nil, contextInfo: nil);
+                        }
+                        else {
+                            self.presentError(folderError);
+                        }
+                    }
+                }
+            });
+        }
+    }
+    
+    func chooseGamesFolderURL(URL:NSURL, inout outError:NSError?) -> Bool {
+        var controller:AppDelegate = NSApplication.sharedApplication().delegate as AppDelegate;
+        var addSampleGames:Bool = self.sampleGamesToggle.state == 1 ? true : false;
+        
+        return controller.assignGamesFolderURL(URL, addSampleGames: addSampleGames, createIfMissing: false, outError: &outError);
+    }
+}
