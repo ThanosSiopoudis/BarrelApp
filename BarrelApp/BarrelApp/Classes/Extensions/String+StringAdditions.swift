5e3443f6c2322c067bf25a12b2a0aa971fc5e8e6
Thanos Siopoudis <siopoudis@me.com>
2014-08-27 09:28:12 +0100

    Extend String to get full path components from a string

---
+++ BarrelApp/BarrelApp/Classes/Extensions/String+StringAdditions.swift
@@ -0,0 +1,31 @@
+//
+//  String+StringAdditions.swift
+//  BarrelApp
+//
+//  Created by Thanos Siopoudis on 26/08/2014.
+//  Copyright (c) 2014 AppCake Limited. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func fullPathComponents() -> NSArray {
+        if (countElements(self) < 1) {
+            return NSArray();
+        }
+
+        var path:String = self.stringByStandardizingPath;
+        var rootPath:String = "/";
+
+        var paths:NSMutableArray = NSMutableArray(capacity: 10);
+        do {
+            paths.addObject(path);
+            path = path.stringByDeletingLastPathComponent;
+        }
+        while (countElements(path) > 0 && path != rootPath);
+
+        // Reverse the array to put the components back in their original order
+        var reverse:NSArray = paths.reverseObjectEnumerator().allObjects;
+        return reverse;
+    }
+}
