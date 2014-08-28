9ad41021f0f45b0318a7fbe328bd8d91b609f1d4
Thanos Siopoudis <siopoudis@me.com>
2014-08-28 01:03:26 +0100

    Add an IBAction to show the games folder

--- BarrelApp/BarrelApp/Classes/Controllers/BLWelcomeViewController.swift
+++ BarrelApp/BarrelApp/Classes/Controllers/BLWelcomeViewController.swift
@@ -16,8 +16,27 @@ class BLWelcomeViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad();
     }
+    
     @IBAction func showPreferences(sender: AnyObject) {
         self.preferencesWindow = BLPreferencesWindowController(windowNibName: "BLPreferences");
         self.preferencesWindow.showWindow(self);
+    }
+    
+    @IBAction func showGamesFolder(sender:AnyObject) {
+        var appdlg:AppDelegate = NSApplication.sharedApplication().delegate as AppDelegate;
+        var URL:NSURL? = appdlg.gamesFolderURL;
+        var revealed:Bool = false;
+        
+        if (URL != nil) {
+            revealed = appdlg.revealURLsInFinder([URL!]);
+        }
+        
+        if (revealed) {
+            // Apply Shelf appearance
+        }
+        else {
+            var window:NSWindow? = sender.window as? NSWindow;
+            appdlg.promptForMissingGamesFolderInWindow(window);
+        }
     }
 }
