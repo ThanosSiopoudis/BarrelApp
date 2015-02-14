31c6b9a2f0098f70b1b03916004bba5705b2db60
Thanos Siopoudis <siopoudis@me.com>
2015-02-14 11:51:46 +0000

    Add RestKit required frameworks and link to main project

--- BarrelApp/BarrelApp/BarrelApp-Bridging-Header.h
+++ BarrelApp/BarrelApp/BarrelApp-Bridging-Header.h
@@ -2,4 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <CoreServices/CoreServices.h>
+#import <RestKit/RestKit.h>
 #import "ObjC-Helpers.h"
