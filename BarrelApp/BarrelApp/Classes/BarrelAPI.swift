27341a92d4f2f2be31ef554efc82d49cead8b68f
Thanos Siopoudis <siopoudis@me.com>
2015-02-14 11:52:21 +0000

    Add class functions for Barrel.API

---
+++ BarrelApp/BarrelApp/Classes/BarrelAPI.swift
@@ -0,0 +1,32 @@
+//
+//  BarrelAPI.swift
+//  BarrelApp
+//
+//  Created by Thanos Siopoudis on 13/02/2015.
+//  Copyright (c) 2015 AppCake Limited. All rights reserved.
+//
+
+import Foundation
+
+class BarrelAPI:NSObject {
+    
+    class func listOfAllEngines(toBlock completionBlock:((RKObjectRequestOperation!, RKMappingResult!) -> Void)!, failBlock errorBlock:((RKObjectRequestOperation!, NSError!) -> Void)!) {
+        var engineMapping:RKObjectMapping = RKObjectMapping(forClass: Engine.self);
+        engineMapping.addAttributeMappingsFromDictionary([
+            "_id"       : "id",
+            "created"   : "Created",
+            "name"      : "Name",
+            "path"      : "Path"
+        ]);
+        
+        var responseDescriptor:RKResponseDescriptor = RKResponseDescriptor(mapping: engineMapping,
+            pathPattern: nil, keyPath: "", statusCodes: RKStatusCodeIndexSetForClass(RKStatusCodeClass.Successful));
+        
+        let url:NSURL = NSURL(string: "http://localhost:3000/engines")!;
+        let urlRequest:NSURLRequest = NSURLRequest(URL: url);
+        var objectRequestOperation:RKObjectRequestOperation = RKObjectRequestOperation(request: urlRequest, responseDescriptors: [responseDescriptor]);
+        objectRequestOperation.setCompletionBlockWithSuccess(completionBlock, failure: errorBlock);
+        
+        objectRequestOperation.start();
+    }
+}
