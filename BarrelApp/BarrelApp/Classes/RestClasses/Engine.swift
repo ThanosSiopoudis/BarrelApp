cd633c95448827b4b2a5d4bad328be3511bf7f62
Thanos Siopoudis <siopoudis@me.com>
2015-02-14 11:52:08 +0000

    Add a main Engine RK descriptor

---
+++ BarrelApp/BarrelApp/Classes/RestClasses/Engine.swift
@@ -0,0 +1,16 @@
+//
+//  Engine.swift
+//  BarrelApp
+//
+//  Created by Thanos Siopoudis on 13/02/2015.
+//  Copyright (c) 2015 AppCake Limited. All rights reserved.
+//
+
+import Foundation
+
+class Engine:NSObject {
+    var id:UInt?
+    var Created:NSDate?
+    var Name:String?
+    var Path:String?
+}
