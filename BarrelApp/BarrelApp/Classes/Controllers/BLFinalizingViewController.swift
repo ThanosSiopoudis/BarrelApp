82f2ee2433eb14b44cb79a423db623a6d4f9462f
Thanos Siopoudis <siopoudis@me.com>
2015-02-26 23:28:05 +0000

    Fix the massive line height of the Avenir Next font

--- BarrelApp/BarrelApp/Classes/Controllers/BLFinalizingViewController.swift
+++ BarrelApp/BarrelApp/Classes/Controllers/BLFinalizingViewController.swift
@@ -11,4 +11,19 @@ import Cocoa
 class BLFinalizingViewController: NSViewController {
     @IBOutlet weak var titleText:NSTextField?
     @IBOutlet var controller:BLImportWindowController?
+    
+    override func awakeFromNib() {
+        var title:String = self.titleText!.stringValue;
+        
+        var textColour:NSColor = NSColor.whiteColor();
+        var theFont:NSFont = NSFont(name: "Avenir Next", size: 32.0)!
+        var textParagraph:NSMutableParagraphStyle = NSMutableParagraphStyle();
+        textParagraph.lineSpacing = 6.0;
+        textParagraph.maximumLineHeight = 38.0;
+        textParagraph.alignment = NSTextAlignment.CenterTextAlignment;
+        
+        var attrDict:NSDictionary = NSDictionary(objectsAndKeys: theFont, NSFontAttributeName, textColour, NSForegroundColorAttributeName, textParagraph, NSParagraphStyleAttributeName);
+        var attrString:NSAttributedString = NSAttributedString(string: title, attributes: attrDict);
+        self.titleText!.attributedStringValue = attrString;
+    }
 }
